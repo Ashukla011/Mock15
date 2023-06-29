c80e44f4978b8a9ddae6c30c7142c866e6ed0715
Ashukla011 <shuklavashu9696@gmail.com>
2023-06-29 11:46:10 +0530

    roxiler

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
-const mongoose=require("mongoose")
 
-mongoose.set('strictQuery', false)
-require("dotenv").config();
-const connection=mongoose.connect('mongodb+srv://kavanish011:super30shukla@cluster0.cqhfocy.mongodb.net/mock15?retryWrites=true&w=majority')
+const mongoose=require("mongoose")
+// require("dotenv").config()
+
+
+const  connection=mongoose.connect("mongodb+srv://kavanish011:super30shukla@cluster0.cqhfocy.mongodb.net/Roxiler?retryWrites=true&w=majority")
 
 module.exports={connection};
